80e6f5be1a42baa53e0a77ab131aa3c8499b8f5d
newlander <ydidya.n@gmail.com>
2022-12-06 00:27:10 +0200

    Optimizing and minimizing the posting part of the code using the dictionary and lambda functions. Also created an additional postByType idea for further development guided for the GUI

--- LinkedIn_script.py
+++ LinkedIn_script.py
@@ -1,41 +1,22 @@
-from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-from time import sleep
-from strings import *
+from postType import *
 
 logfile = open(log_file_path, 'a', encoding='utf-8')
 post_content = open(post_file_path, 'r', encoding='utf-8').readlines()
 
 
-def report_start():
-    now = datetime.now().strftime("%m/%d/%Y | %H:%M:%S")
-    logfile.write(log_report_start + now + '\n')
-
-
-def post_success():
-    now = datetime.now().strftime(time_format)
-    logfile.write(log_report_success + now + '\n')
-    raise Exception(exception_report_success + now + '\n')
-
-
-def post_fail():
-    now = datetime.now().strftime(time_format)
-    logfile.write(log_report_fail + now + '\n')
-    raise Exception(exception_report_fail + now + '\n')
-
-
 def post_process():
     post_test = post_content
     if post_test[0] != post_head:
+        logging.exception(exception_post_border_error)
         raise Exception(exception_post_border_error)
     post_test.pop(0)
     post_number = post_test[0].split()
     if not post_number[-1].isnumeric():
+        logging.exception(exception_post_number_error)
         raise Exception(exception_post_number_error)
-    now = datetime.now().strftime(time_format)
-    logfile.write(log_report_valid + now + '\n')
+    logging.info(log_report_valid)
 
 
 def tag_name(post_input, name_to_comment):
@@ -47,17 +28,19 @@ def tag_name(post_input, name_to_comment):
     sleep(2)
 
 
-def hash_tag(post_input, name_to_comment):
-    hashtag = name_to_comment.replace(" ", "_")
-    post_input.send_keys("#" + hashtag)
-    sleep(1)
-    post_input.send_keys(Keys.ARROW_DOWN)
-    sleep(1)
-    post_input.send_keys(Keys.ENTER)
-    sleep(2)
+def addHashTag(post_input):
+    names = ["teaching and learning", "software development", "כולא_לייק", "ראובוט"]
+    longHashTag = ""
+    for name in names:
+        hashtag = name.replace(" ", "_")
+        longHashTag += ("#" + hashtag + " ")
+    post_input.send_keys(longHashTag)
 
 
 def clean_post():
+    """
+    Removes the already published post from the post list, so the file won't be too big or cause copy errors.
+    """
     update_file = open(post_file_path, encoding='utf-8')
     data = update_file.read()
     data = data[data.index(post_end) + len(post_end):]
@@ -67,44 +50,48 @@ def clean_post():
 
 
 def post_to_linkedIn():
+    counter = 0
     driver = webdriver.Chrome(executable_path=webdriver_path)
-    linkedin = linkedin_path
     userdata = open(userdata_file_path).readlines()
+    commands = {
+        "0user name:": lambda: driver.find_element(By.ID, username_field).send_keys(userdata[0]),
+        "1password": lambda: driver.find_element(By.ID, password_field).send_keys(userdata[1]),
+        "2login button": lambda: driver.find_element(By.XPATH, login_button).click(),
+        "3find typing area": lambda: driver.find_element(By.XPATH, writing_button).click(),
+        "@upload": uploadToLinkedin(driver, "image", image_path),
+        "@final click": lambda: driver.find_element(By.XPATH, post_button).click(),
+        "closing time": lambda: driver.close()
+    }
     try:
         driver.get(linkedin)
-        sleep(1)
-        driver.find_element(By.ID, username_field).send_keys(userdata[0])
-        driver.find_element(By.ID, password_field).send_keys(userdata[1])
-        driver.find_element(By.XPATH, login_button).click()
-        sleep(3)
-        driver.find_element(By.XPATH, writing_button).click()
-        sleep(2)
-        post_input = driver.find_element(By.CLASS_NAME, posting_field)
-        for line in post_content:
-            if line == post_end:
-                break
-            if not line == post_end:
-                post_input.send_keys(line)
-        sleep(2)
-        hash_tag(post_input,"ראובוט")
-        # driver.find_element(By.XPATH, post_button).click() # deactivated. to re-activate - remove # sign
-        post_success()
-        driver.close()
+        waitList = [1, 1, 1, 2, 3, 3, 4, 4]
+        for command in commands.values():
+            counter += 1
+            command()
+            if counter == 5:
+                post_input = driver.find_element(By.XPATH, posting_field)
+                for line in post_content:
+                    if line == post_end:
+                        break
+                    if not line == post_end:
+                        post_input.send_keys(line)
+                addHashTag(post_input)
+            sleep(waitList[counter])
+        logging.info(log_report_success)
         return
     except Exception as e:
         print(e)
 
 
 def main():
-    report_start()
+    logging.info(log_report_start)
     clean_post()
     try:
         post_process()
         post_to_linkedIn()
-        now = datetime.now().strftime(time_format)
-        logfile.write(log_report_update + now + '\n')
+        logging.info(log_report_update)
     except:
-        post_fail()
+        logging.info(log_report_fail)
 
 
 if __name__ == "__main__":
--- strings.py
+++ strings.py
@@ -1,3 +1,5 @@
+import logging
+
 post_head = "&&&&&\n"
 post_end = "$$$$$\n"
 post_content_end = "%%%%%"
@@ -7,7 +9,7 @@ log_file_path = "logfile.txt"
 post_file_path = "post_content.txt"
 userdata_file_path = "login_data.txt"
 webdriver_path = "/Users/ydidy/Documents/Coding/Web bot/chromedriver"
-linkedin_path = "https://www.linkedin.com/"
+linkedin = "https://www.linkedin.com/"
 
 log_report_start = "posting script STARTED at "
 log_report_stop = "posting script TERMINATED at "
@@ -23,7 +25,20 @@ exception_post_number_error = "Post Error: invalid counting format. Script termi
 
 username_field = "session_key"
 password_field = "session_password"
-posting_field = "ql-editor.ql-blank"
+posting_field = '//div[2]//div[1]/div[2]//div[2]/div/div/div[1]'
 login_button = '//form/button[1]'
 writing_button = "//*[@id = 'main']/div[1]/div/div[1]/button"
 post_button = "//div[@class = 'share-box_actions']//span[@class = 'artdeco-button__text']"
+image_button = "//div[2]//div[2]/div[2]//span[1]/button"
+image_select = '//div[3]/div/div/div[2]/div/div/input'
+image_path = "C:/image.gif"
+video_button = "//div[3]//div[2]//span[2]/button"
+video_select = '//div[3]//div[2]//div[1]/div/input'
+video_path = "C:/image.gif"
+file_complete = '//div[3]//div[2]//div[2]/div/button[2]'
+
+logging.basicConfig(filename=log_file_path,
+                    filemode='a',
+                    format='%(message)s %(asctime)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
